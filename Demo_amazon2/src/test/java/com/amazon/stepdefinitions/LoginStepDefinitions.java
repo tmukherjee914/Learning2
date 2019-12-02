3716b42561394edf5b71517acee7809d0f0af2b0
tmukherjee914 <57941092+tmukherjee914@users.noreply.github.com>
2019-12-02 12:47:51 +0530

    Update LoginStepDefinitions.java

--- Demo_amazon2/src/test/java/com/amazon/stepdefinitions/LoginStepDefinitions.java
+++ Demo_amazon2/src/test/java/com/amazon/stepdefinitions/LoginStepDefinitions.java
@@ -22,87 +22,87 @@ public class LoginStepDefinitions
 {
 	Properties properties = PropertiesFile.getPropertiesFile();
 	String url = properties.getProperty("url");
-	@When("^The user is on the home page$")
-	public void The_user_is_on_the_home_page() {
+// 	@When("^The user is on the home page$")
+// 	public void The_user_is_on_the_home_page() {
 		
-		driver.get(url);
-	};
+// 		driver.get(url);
+// 	};
 	
 	
-	@Then("^Verify page title$")
-	public void Verify_page_title() {
+// 	@Then("^Verify page title$")
+// 	public void Verify_page_title() {
 	
-	    String actual = driver.getTitle();
-	    String Exp = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+// 	    String actual = driver.getTitle();
+// 	    String Exp = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
 	 
-	  //  Assert.assertEquals( Exp	,actual);
+// 	  //  Assert.assertEquals( Exp	,actual);
 	   
-	    SoftAssertions sa = new SoftAssertions();
-	    sa.assertThat(actual).isEqualToIgnoringCase(Exp);
-	    System.out.println("------------------not working----------------------------");
+// 	    SoftAssertions sa = new SoftAssertions();
+// 	    sa.assertThat(actual).isEqualToIgnoringCase(Exp);
+// 	    System.out.println("------------------not working----------------------------");
 		
-	    sa.assertAll();
+// 	    sa.assertAll();
 	
-	};
+// 	};
 		
 	
-	@Given("^The user is on the website page$")
-	public void The_user_is_on_the_website_page() {
+// 	@Given("^The user is on the website page$")
+// 	public void The_user_is_on_the_website_page() {
 	
-		driver.get(url);
+// 		driver.get(url);
 		
-			};
+// 			};
 	
-	@When("^user clicks on the Login button$")
-	public void  user_clicks_on_the_Login_button() 
-	{
-         //Wait.implicitwait(10);
-		Wait.expicitwait(15, loginPageInstance.loginbutton);
-	    Actions act = new Actions(driver);
-	    act.doubleClick(loginPageInstance.loginbutton).build().perform();
-	};
+// 	@When("^user clicks on the Login button$")
+// 	public void  user_clicks_on_the_Login_button() 
+// 	{
+//          //Wait.implicitwait(10);
+// 		Wait.expicitwait(15, loginPageInstance.loginbutton);
+// 	    Actions act = new Actions(driver);
+// 	    act.doubleClick(loginPageInstance.loginbutton).build().perform();
+// 	};
 							
-	@And("^user enter email$")
-	public void user_enter_email()	
-	{
-		String email = properties.getProperty("email");
-		Wait.expicitwait(15, loginPage.emailTxtBox);
-		loginPage.emailTxtBox.sendKeys(email);		
+// 	@And("^user enter email$")
+// 	public void user_enter_email()	
+// 	{
+// 		String email = properties.getProperty("email");
+// 		Wait.expicitwait(15, loginPage.emailTxtBox);
+// 		loginPage.emailTxtBox.sendKeys(email);		
 		
 	   
-	};
-	@And("^clicks on continue$")
-	public void clicks_on_continue()
-	{
+// 	};
+// 	@And("^clicks on continue$")
+// 	public void clicks_on_continue()
+// 	{
 
-		Wait.implicitwait(10);	
-		loginPage.continueLogin.click();
+// 		Wait.implicitwait(10);	
+// 		loginPage.continueLogin.click();
 	  
-	};
-   @And("^user enter password$")
-	public void user_enter_password()
+// 	};
+//    @And("^user enter password$")
+// 	public void user_enter_password()
 	
-	{
-	   String pwd = properties.getProperty("passward");
-		Wait.implicitwait(10);	
-		loginPage.password.sendKeys(pwd);		
-		//System.out.println(password);
+// 	{
+// 	   String pwd = properties.getProperty("passward");
+// 		Wait.implicitwait(10);	
+// 		loginPage.password.sendKeys(pwd);		
+// 		//System.out.println(password);
 	   
-	};
-	@And("^user click on Login$")
-	public void user_click_on_Login()
-	{
+// 	};
+// 	@And("^user click on Login$")
+// 	public void user_click_on_Login()
+// 	{
 
-		Wait.implicitwait(10);	
-		loginPage.loginButton.click();
+// 		Wait.implicitwait(10);	
+// 		loginPage.loginButton.click();
 	  
-	};
+// 	};
 
-	@Then("^user should be logged into the application$")
-	public void user_should_be_logged_into_the_application()
-	{
+// 	@Then("^user should be logged into the application$")
+// 	public void user_should_be_logged_into_the_application()
+// 	{
 	    
-	}
+// 	}
 	
 	
 }
