917ccb48b103e65c7d9a4f0cada029e38feaa37a
tmukherjee914 <57941092+tmukherjee914@users.noreply.github.com>
2019-12-02 12:47:16 +0530

    Update ItemSearchDefinition.java

--- Demo_amazon2/src/test/java/com/amazon/stepdefinitions/ItemSearchDefinition.java
+++ Demo_amazon2/src/test/java/com/amazon/stepdefinitions/ItemSearchDefinition.java
@@ -22,49 +22,49 @@ import static com.amazon.Runnerclass.searchitem;
 public class ItemSearchDefinition {
 	Properties properties = PropertiesFile.getPropertiesFile();
 	
-	@Given("^user is on Home page$") 
-	    public void user_is_on_Home_page() throws InterruptedException {
+// 	@Given("^user is on Home page$") 
+// 	    public void user_is_on_Home_page() throws InterruptedException {
 		 
-		 String url = properties.getProperty("url");
-		driver.get(url);
-		Thread.sleep(1000);
-	};
+// 		 String url = properties.getProperty("url");
+// 		driver.get(url);
+// 		Thread.sleep(1000);
+// 	};
 	
 	  
-	@When("^user enter keyward$")
-	public void user_enter() throws InterruptedException	 
-	{
-		Thread.sleep(1000);
-		 String keyward = properties.getProperty("keyward");
-		searchitem.searchfield.sendKeys(keyward);
+// 	@When("^user enter keyward$")
+// 	public void user_enter() throws InterruptedException	 
+// 	{
+// 		Thread.sleep(1000);
+// 		 String keyward = properties.getProperty("keyward");
+// 		searchitem.searchfield.sendKeys(keyward);
 		
-		Thread.sleep(3000);
+// 		Thread.sleep(3000);
 		
-		searchitem.searchfield.sendKeys(Keys.DOWN);
-		searchitem.searchfield.sendKeys(Keys.DOWN);
-		searchitem.searchfield.sendKeys(Keys.DOWN);
+// 		searchitem.searchfield.sendKeys(Keys.DOWN);
+// 		searchitem.searchfield.sendKeys(Keys.DOWN);
+// 		searchitem.searchfield.sendKeys(Keys.DOWN);
 		
-		Thread.sleep(2000);
+// 		Thread.sleep(2000);
 		
-		searchitem.searchfield.sendKeys(Keys.ENTER);
-		Thread.sleep(5000);
-	};
+// 		searchitem.searchfield.sendKeys(Keys.ENTER);
+// 		Thread.sleep(5000);
+// 	};
 	    
-	@And("^user select on third option from the list$") 
+// 	@And("^user select on third option from the list$") 
 	
-	public void user_select_on_third_option_from_the_list() throws AWTException, InterruptedException {
+// 	public void user_select_on_third_option_from_the_list() throws AWTException, InterruptedException {
 		
-		Thread.sleep(1000);	
-	 // searchitem.searchsuggest.get(2).click();
-	  Thread.sleep(1000);
+// 		Thread.sleep(1000);	
+// 	 // searchitem.searchsuggest.get(2).click();
+// 	  Thread.sleep(1000);
 		
 		
-	};
+// 	};
 
-	@Then("^user should navigates to item detail page$")
-	public void user_should_navigates_to_item_detail_page()
-	{
-	   System.out.println("Changes in branch logout ");
-	};
+// 	@Then("^user should navigates to item detail page$")
+// 	public void user_should_navigates_to_item_detail_page()
+// 	{
+// 	   System.out.println("Changes in branch logout ");
+// 	};
 
 }
